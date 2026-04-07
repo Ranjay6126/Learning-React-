fff4ceb75285315cd22d0ed0a3f0bc0c4e33d91f
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-08 01:14:16 +0530

    know the power of the {}

--- app.js
+++ app.js
@@ -19,13 +19,35 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
 
+let number = 1000;
 
 // React functional component //
 
 const HeadingComponent = () =>{
     return <h1> Hello react using functional component </h1>
 }
+  
+
+const Heading2 = ()  =>(
+
+    <div>
+        <HeadingComponent />
+      <h2>{number}</h2>
+        <h1> Hello react using functional component Heading2  </h1>
+    </div>
+);
 
 
+const Root1 = () => (
+    <div className="container">
+<h2>hello ji this one is another syntax</h2>
+    </div>
+); 
+
+
+/*
+root.render(<HeadingComponent />);
+root.render(<Root1 />);*/
+root.render(<Heading2 />);
 
 
