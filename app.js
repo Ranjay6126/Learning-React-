a7308a1702a38a9161066092b5eda53cf66c34cb
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-07 23:41:00 +0530

    add the comments

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const heading = React.createElement("h1", { id: "heading" }, "Namaste React 🙆
 
 console.log(heading);
 
+/*
+JSX:=> it is not html in js. It is look like html syntex
+
+// JSX (transpiled before it reaches the js ) === PARCEL == Babel------------>>>>( babel /  it js compiler )
+
+const heading = <h1> Hello react using jsx </h1>; */ 
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
