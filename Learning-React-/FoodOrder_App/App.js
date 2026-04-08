38faee82f6ff1429fbe616131dca9fead03d4b12
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-08 23:53:00 +0530

    app.js update

---
+++ Learning-React-/FoodOrder_App/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/* 
+Header
+logo
+nav items
+body- search bar / resto container >> resturant card 
+footer  // copyrights, links, address, contact info     
+*/
+
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+
+            //header
+            
+        </div>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout/>)
